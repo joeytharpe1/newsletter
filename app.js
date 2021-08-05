0689cbd6025fe4c9ab936ad71a863cb288577c84
Joseph Tharpe <solowun6@aol.com>
2021-08-05 00:02:28 -0700

    version 1

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const request = require('request');
+const https = require('https');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+})
+
+app.post('/', (req, res) => {
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = `https://us5.api.mailchimp.com/3.0/lists/c244efe37e`;
+
+    const options = {
+        method: 'POST',
+        auth: 'joeytharpe1:fabd6fdd52212478b800870262e00cba-us5'
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + `/success.html`)
+        } else {
+            res.sendFile(__dirname + `/failure.html`)
+        }
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.post('/failure', (req, res) => {
+    res.redirect('/')
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`server running on port 3000`)
+})
+
+// apiKey=fabd6fdd52212478b800870262e00cba-us5
+//listId = c244efe37e
